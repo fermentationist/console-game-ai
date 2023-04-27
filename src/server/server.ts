63cc5adef4466d7700fb6768c996174eb80b2008
Dennis Hodges <fermentationist@gmail.com>
2023-04-27 17:21:31 -0500

    Added woke-dyno to server to keep server awake

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import wakeDyno from "woke-dyno";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
 import apiRouter from "./api/routes/index.js";
 import rateLimiter from "./rateLimiter.js";
 
 const API_SERVER_PORT = process.env.API_SERVER_PORT || 8080;
-
+const WAKE_SERVER_INTERVAL = process.env.WAKE_SERVER_INTERVAL || 14 * 60 * 1000; // 14 minutes
 const STATIC_FOLDER = "../game/";
 const filename = fileURLToPath(import.meta.url);
 const dirName = dirname(filename);
@@ -27,6 +28,16 @@ app.use("/*", (req, res) => {
 
 app.listen(API_SERVER_PORT, () => {
   console.log(`Express app listening on port ${API_SERVER_PORT}`);
+  const offset = 4; // NY
+  const getOffsetHours = (hours: number) => (hours + offset) > 24 ? 24 - (hours + offset) : hours + offset;
+  const napStartHour = getOffsetHours(22);
+  const napEndHour = getOffsetHours(7)
+  wakeDyno({
+    url: `http://localhost:${API_SERVER_PORT}`,
+    interval: WAKE_SERVER_INTERVAL, 
+    startNap: [napStartHour, 0, 0, 0],
+    endNap: [napEndHour, 0, 0, 0]
+  }).start();
 });
 
 export default app;
