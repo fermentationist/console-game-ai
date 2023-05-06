49f425136cb015a7e2c8fbce77f68011fc4f61c5
Dennis Hodges <fermentationist@gmail.com>
2023-05-06 11:20:04 -0500

    Updated server.ts to only use woke-dyno if WAKE_SERVER_URL env var is set.

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,7 +7,7 @@ import apiRouter from "./api/routes/index.js";
 import rateLimiter from "./rateLimiter.js";
 
 const API_SERVER_PORT = process.env.API_SERVER_PORT || 8080;
-const WAKE_SERVER_URL = process.env.WAKE_SERVER_URL || `http://localhost:${API_SERVER_PORT}`;
+const WAKE_SERVER_URL = process.env.WAKE_SERVER_URL;
 const WAKE_SERVER_INTERVAL = process.env.WAKE_SERVER_INTERVAL || 14 * 60 * 1000; // 14 minutes
 const STATIC_FOLDER = "../game/";
 const filename = fileURLToPath(import.meta.url);
@@ -29,16 +29,18 @@ app.use("/*", (req, res) => {
 
 app.listen(API_SERVER_PORT, () => {
   console.log(`Express app listening on port ${API_SERVER_PORT}`);
-  const offset = 4; // NY
-  const getOffsetHours = (hours: number) => (hours + offset) > 24 ? Math.abs(24 - (hours + offset)) : hours + offset;
-  const napStartHour = getOffsetHours(22);
-  const napEndHour = getOffsetHours(7)
-  wakeDyno({
-    url: WAKE_SERVER_URL,
-    interval: WAKE_SERVER_INTERVAL, 
-    startNap: [napStartHour, 0, 0, 0],
-    endNap: [napEndHour, 0, 0, 0]
-  }).start();
+  if (WAKE_SERVER_URL) {
+    const offset = 4; // NY
+    const getOffsetHours = (hours: number) => (hours + offset) > 24 ? Math.abs(24 - (hours + offset)) : hours + offset;
+    const napStartHour = getOffsetHours(22);
+    const napEndHour = getOffsetHours(7)
+    wakeDyno({
+      url: WAKE_SERVER_URL,
+      interval: WAKE_SERVER_INTERVAL, 
+      startNap: [napStartHour, 0, 0, 0],
+      endNap: [napEndHour, 0, 0, 0]
+    }).start();
+  }
 });
 
 export default app;
